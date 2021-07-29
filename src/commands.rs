a3be99079e9a644eaeaef857c52d7eebcc77e77e
Santo Cariotti <santo@dcariotti.me>
2021-07-29 18:44:22 +0200

    fix: typo on text descripion of a command

--- src/commands.rs
+++ src/commands.rs
@@ -5,7 +5,7 @@ use teloxide::utils::command::BotCommand;
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
 pub enum Command {
-    #[command(description = "display this text")]
+    #[command(description = "Display this text")]
     Help,
 }
 
